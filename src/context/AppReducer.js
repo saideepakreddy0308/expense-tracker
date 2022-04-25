196bfa53c6cce1a2ca218335f6316223c299acc0
Deepak <saideepakreddy0308@gmail.com>
2022-04-25 23:10:23 +0530

    small changes

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,4 @@
-import { Transaction } from "../components/Transaction";
-
-export default(state,action) => {
+export default (state,action) => {
     switch (action.type){
         case 'DELETE_TRANSACTION':
             return{
